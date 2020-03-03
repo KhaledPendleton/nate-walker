d3bcc19728cf4bb39949e64c8dbcb3a9df9a01a2
Khaled Pendleton <khaled@Khaleds-MacBook-Pro.local>
2020-03-02 20:29:40 -0500

    Styled all portfolio project pages

--- src/routes/portfolio/[slug].svelte
+++ src/routes/portfolio/[slug].svelte
@@ -15,4 +15,39 @@
     export let project;
 </script>
 
-{ @html project.html }
+<style>
+    :global(.project) { font-size: 1.2rem; }
+
+    :global(.project h1),
+    :global(.project h2),
+    :global(.project h3),
+    :global(.project h4),
+    :global(.project h5),
+    :global(.project h6) {
+        margin-top: 0.5em;
+        margin-bottom: 0.25em;
+    }
+
+    :global(.project h1) { font-size: 2em; }
+    :global(.project h2) { font-size: 1.5em; }
+    :global(.project h3) { font-size: 1.17em; }
+    :global(.project h4) { font-size: 1em; }
+    :global(.project h5) { font-size: 0.83em; }
+    :global(.project h6) { font-size: 0.67em; }
+
+    :global(.project pre),
+    :global(.project code) {
+        padding-left: 0.5em;
+        padding-right: 0.5em;
+        background-color: hsl(0, 0%, 93%);
+        border: 1px solid hsl(0, 0%, 60%);
+        border-radius: 5px;
+    }
+
+    :global(.project pre) { padding: 0.5em; }
+    :global(.project pre > code) { all: unset; }
+</style>
+
+<article class="project">
+    { @html project.html }
+</article>
