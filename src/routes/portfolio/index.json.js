c13366489be500c1f160ce79bc5658e6cce7b4a6
Khaled Pendleton <hi@khaledpendleton.com>
2020-05-17 10:27:01 -0400

    Added sort capabilities to projects getter

--- src/routes/portfolio/index.json.js
+++ src/routes/portfolio/index.json.js
@@ -6,6 +6,11 @@ export function get(req, res) {
     if (!contents || process.env.NODE_ENV !== 'production') {
         const projects = getProjects()
             .filter(post => !post.metadata.draft)
+            .sort((a, b) => {
+                const aPos = a.metadata.position || 0;
+                const bPos = b.metadata.position || 0;
+                return aPos - bPos;
+            })
             .map(({ slug, metadata }) => ({ slug, metadata }));
 
         contents = JSON.stringify(projects);
